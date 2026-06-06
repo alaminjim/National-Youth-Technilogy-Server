3e9890d689bd10ecb49d7749f0218c97418fcc1a
Al-Amin Islam <jimalamin7@gmail.com>
2026-06-06 16:00:01 +0600

    successfully full project ready

---
+++ src/config/cors.ts
@@ -0,0 +1,17 @@
+import createCors from "cors";
+import { envVars } from "./env";
+
+const origin = [
+  envVars.FRONTEND_URL as string,
+  "http://localhost:3000",
+  "https://technical-education-technilogy.vercel.app",
+];
+
+const cors = createCors({
+  origin,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+});
+
+export { cors };
